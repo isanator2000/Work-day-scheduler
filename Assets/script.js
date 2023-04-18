72c4ad9e87837f348acf64f3fb0ca871e4515f3c
Isa <iruizord@gmail.com>
2023-04-17 19:25:17 -0600

    adding variables to global scope

--- Assets/script.js
+++ Assets/script.js
@@ -1,3 +1,16 @@
+// global variables
+var hrsStart = moment().startOf('day').add(7,'h');
+var totalHrs = 10;
+var currentHrs = moment().format('H');
+var timeTable;
+var currentTime;
+
+
+
+
+
+
+
 
 $(function () {
 
