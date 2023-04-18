e30c3400b370d85c65128281db08e975acf25f4d
Isa <iruizord@gmail.com>
2023-04-17 19:46:30 -0600

    adding functions

--- Assets/script.js
+++ Assets/script.js
@@ -10,3 +10,31 @@ function showcurrentDay() {
   $('#currentDay').text(today);
 }
 
+function fillTimeTable() {
+
+  for (var hour = 0; hour < totalHours; hour++) { 
+      var actHour = hour + 8;
+      timeTable = hourBeginning.add(1,'h').format('HH:mm A');
+      if (currentHrs == actHour) {
+          currentTime = 'present';
+      } else if (currentHrs < actHour) {
+          currentTime = 'past';
+      } else {
+          currentTime= 'future';
+      }
+
+      var appendedText = 
+      `<div id="hour-${actHour}" class="row time-block ${currentTime}">
+          <div class="col-md-1 hour">${timeTable}</div>
+          <textarea class="col-md-10 description ${actHour}"></textarea>
+          <button class="btn saveBtn col-md-1">
+              <i class="fas fa-save"></i>
+          </button>
+      </div>`;
+
+  $(".container").append(appendedText);
+
+}
+
+loadSchedule();
+}
